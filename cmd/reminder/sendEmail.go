e94984665defedfc8e89f101c4cb30cf19f58c90
Somya Sanu <somyasanu@192.168.1.2>
2025-09-13 19:59:34 +0530

    [feat] added logic to send reminder using gmail.

--- cmd/reminder/sendEmail.go
+++ cmd/reminder/sendEmail.go
@@ -13,7 +13,7 @@ func sendHtmlMail(name string, to string, date int8, month string, mobile int64)
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	body := fmt.Sprintf("<html><body><h1>Reminder for %s's birthday</h1><p>Do wish them on %d %s</p><p>Contact them on %d</p></body></html>", name, date, month, mobile)
 	msg := []byte(subject + mime + body)
-	err := smtp.SendMail("smtp.gmail.com:587", auth, os.Getenv("from_mail"), []string{to}, msg)
+	err := smtp.SendMail("smtp.gmail.com:587", auth, os.Getenv("mail"), []string{to}, msg)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
